b5f6b25398436f9512a0c654f336da327be9c5fc
rakotonjatovo <96283491+rakotonjatovo@users.noreply.github.com>
2022-05-07 17:13:25 +0300

    Add files via upload

---
+++ SAYNA-JAVASCRIPT-NOWAYHOME-042022/SAYNA-HTMLCSS-NOWAYHOME-032022-main/Acceuil.js
@@ -0,0 +1,19 @@
+
+//changement coulor et background passage souris
+function change() {
+    document.getElementById('btn_change').style.backgroundColor='white';
+    document.getElementById('btn_change').style.color="#540a0a";
+    document.getElementById('btn_change').style.fontWeight="700";
+}
+
+function normal() {
+    document.getElementById('btn_change').style.backgroundColor='#540a0a';
+    document.getElementById('btn_change').style.color="white";
+    document.getElementById('btn_change').style.fontWeight="700";
+}
+
+//hover sur le footer
+
+function changefooter() {
+    Document.getElementById("footer").style.color="white";
+}
